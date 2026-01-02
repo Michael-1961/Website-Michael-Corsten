c9b591105464cf2b771b14bac6682b6eb2d803b5
Jan Corsten <7761711+jancurse@users.noreply.github.com>
2026-01-02 15:10:37 +0000

    deploy website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import rehypeKatex from 'rehype-katex';
 import rehypeSlug from 'rehype-slug';
 
 export default defineConfig({
+  site: 'https://jancurse.github.io/Website-Michael-Corsten/',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'de',
